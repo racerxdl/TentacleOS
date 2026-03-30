c634eec0e7c24d373ef09d83fc5222f4024c8c8d
luis_thiago <95657866+lthiagovs@users.noreply.github.com>
2026-03-30 12:28:46 -0300

    feat(ui): new dropdown component

---
+++ firmware_p4/components/Applications/ui/components/dropdown/include/dropdown_ui.h
@@ -0,0 +1,11 @@
+#ifndef UI_DROPDOWN_H
+#define UI_DROPDOWN_H
+
+#include "lvgl.h"
+
+void dropdown_ui_create(lv_obj_t * parent);
+void dropdown_ui_register_hide_objs(lv_obj_t ** objs, int count);
+bool dropdown_ui_is_open(void);
+void dropdown_ui_raise(void);
+
+#endif
