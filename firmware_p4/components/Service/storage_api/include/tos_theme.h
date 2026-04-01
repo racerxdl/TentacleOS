4364cc900c9c284f65f32ded1c9ef31aadbf8025
luis_thiago <95657866+lthiagovs@users.noreply.github.com>
2026-03-31 21:49:18 -0300

    feat(storage): add tos_theme

---
+++ firmware_p4/components/Service/storage_api/include/tos_theme.h
@@ -0,0 +1,7 @@
+#ifndef TOS_THEME_H
+#define TOS_THEME_H
+
+#include "esp_err.h"
+void tos_theme_load_from_sd(void);
+
+#endif // TOS_THEME_H
---
+++ firmware_p4/components/Service/storage_api/tos_theme.c
@@ -0,0 +1,6 @@
+#include "tos_theme.h"
+#include "ui_theme.h"
+
+void tos_theme_load_from_sd(void) {
+    ui_theme_load_from_sd();
+}
